3aa2c6f66fe26acf0f3ea81a9035ec740970ebb5
santiarroyave <santi98cv@hotmail.com>
2023-08-24 18:50:26 +0200

    Crea estructura de directorios y añade modelo Cliente

---
+++ T30-Ejercicio01/Models/Cliente.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Conventions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T30_Ejercicio01.Models
+{
+    internal class Cliente
+    {
+        //ATRIBUTOS
+        public int id { get; set; }
+        public string name { get; set; }
+        public string apellido { get; set; }
+        public string direccion { get; set; }
+        public int dni { get; set; }
+        public DateTime fecha { get; set; }
+    }
+}
